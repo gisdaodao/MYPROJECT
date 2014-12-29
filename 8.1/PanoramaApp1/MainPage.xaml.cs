d95ef9cb56d4539f75e86e53402c3b5f63115aef
gisdaodao <asd1234>
2014-12-29 12:49:37 +0800

    o

--- 8.1/PanoramaApp1/MainPage.xaml.cs
+++ 8.1/PanoramaApp1/MainPage.xaml.cs
@@ -8,28 +8,37 @@ using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
 using 股票新闻;
-
+using System.IO.IsolatedStorage;
+using Microsoft.Phone.Tasks;
 namespace PanoramaApp1
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        IsolatedStorageSettings settings = IsolatedStorageSettings.ApplicationSettings;
         // 构造函数
         public MainPage()
         {
             InitializeComponent();
-            file.Add(new Info() { text = "美丽说", dataurl = "http://m.meilishuo.com" });
-            file.Add(new Info() { text = "糯米网", dataurl = "http://m.nuomi.com/" });
+
+            file.Add(new Info() { text = "淘宝网", dataurl = "http://m.taobao.com/" });
+            file.Add(new Info() { text = "天猫网", dataurl = "http://m.tmall.com/" });
             file.Add(new Info() { text = "京东", dataurl = "http://m.jd.com/" });
-            file.Add(new Info() { text = "拍拍网", dataurl = "http://www.paipai.com/m2/index.html" });
-            file.Add(new Info() { text = "天猫网", dataurl = "http://www.tmall.com/" });
-            file.Add(new Info() { text = "蘑菇街", dataurl = "http://m.mogujie.com/" });
             file.Add(new Info() { text = "苏宁易购", dataurl = "http://m.suning.com/" });
+            file.Add(new Info() { text = "拍拍网", dataurl = "http://www.paipai.com/m2/index.html" });
+                
+            file.Add(new Info() { text = "蘑菇街", dataurl = "http://m.mogujie.com/" });
+          
             file.Add(new Info() { text = "国美在线", dataurl = "http://m.gome.com.cn/" });
             file.Add(new Info() { text = "1号店", dataurl = "http://m.yhd.com/" });
             file.Add(new Info() { text = "亚马逊", dataurl = "http://www.amazon.cn/" });
+            file.Add(new Info() { text = "唯品会", dataurl = "http://m.vip.com/" });
             file.Add(new Info() { text = "聚划算", dataurl = "http://jhs.m.taobao.com/m/index.htm" });
-            file.Add(new Info() { text = "e龙旅行", dataurl = "http://m.elong.com/" });
-            file.Add(new Info() { text = "酒仙网", dataurl = "http://m.jiuxian.com/" });
+            file.Add(new Info() { text = "当当网", dataurl = "http://m.dangdang.com/" });
+            file.Add(new Info() { text = "易迅网", dataurl = "http://m.51buy.com/t" });
+            //file.Add(new Info() { text = "e龙旅行", dataurl = "http://m.elong.com/" });
+            // file.Add(new Info() { text = "酒仙网", dataurl = "http://m.jiuxian.com/" });
+            //   file.Add(new Info() { text = "美丽说", dataurl = "http://m.meilishuo.com" });
+            file.Add(new Info() { text = "糯米网", dataurl = "http://m.nuomi.com/" });
             //file.Add(new Info() { text = "唯品会", dataurl = "http://m.vip.com/" });
             //file.Add(new Info() { text = "小荷特卖", dataurl = "http://m.xiaoher.com/" });
             //file.Add(new Info() { text = "聚美优品", dataurl = "http://m.jumei.com/" });
@@ -72,12 +81,82 @@ namespace PanoramaApp1
         }
         List<Info> file = new List<Info>();
         // 为 ViewModel 项加载数据
+        //protected override void OnNavigatedTo(NavigationEventArgs e)
+        //{
+        //    //if (!App.ViewModel.IsDataLoaded)
+        //    //{
+        //    //    App.ViewModel.LoadData();
+        //    //}
+        //}
+
+        private void TextBlock_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+
+            MarketplaceReviewTask p = new MarketplaceReviewTask(); p.Show();
+        }
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            //if (!App.ViewModel.IsDataLoaded)
-            //{
-            //    App.ViewModel.LoadData();
-            //}
+            try
+            {
+                if (e.IsNavigationInitiator == false && e.NavigationMode == NavigationMode.Back)
+                {
+
+                    int i = 1;
+                    if (!settings.Contains("guanggao"))
+                    {
+                        settings.Add("guanggao", i);
+                        settings.Save();
+                    }
+                    else
+                    {
+                        int k = (int)settings["guanggao"];
+                        k = k + i;
+                        settings["guanggao"] = k;
+                        settings.Save();
+                        sethide();
+                    }
+
+
+
+                }
+                if (e.NavigationMode == NavigationMode.New)
+                {
+                    if (!settings.Contains("guanggao"))
+                    {
+                        settings.Add("guanggao", 0);
+                        settings.Save();
+                    }
+                    else
+                    {
+                        int k = (int)settings["guanggao"];
+                        if (k >=2)
+                        {
+                            sethide();
+                           
+                        }
+                        //k = k + i;
+                        //settings["guanggao"] = k;
+                        // settings.Save();
+                    }
+
+
+
+                }
+
+                base.OnNavigatedTo(e);
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+            //this.surfaceAdImageXaml.InitAdControl(AdModeType.Normal);
+            // this.surfaceAdImageXaml.InitAdControl(AdModeType.Debug); 
         }
+        private void sethide()
+        {
+            adpanel.Visibility = Visibility.Collapsed;
+        }
+
     }
 }
