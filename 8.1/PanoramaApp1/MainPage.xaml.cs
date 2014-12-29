5a7d4023b729c08cbe9f22588d03027d0620195f
gisdaodao <asd1234>
2014-12-29 13:44:55 +0800

    ok

--- 8.1/PanoramaApp1/MainPage.xaml.cs
+++ 8.1/PanoramaApp1/MainPage.xaml.cs
@@ -20,25 +20,27 @@ namespace PanoramaApp1
         {
             InitializeComponent();
 
-            file.Add(new Info() { text = "淘宝网", dataurl = "http://m.taobao.com/" });
-            file.Add(new Info() { text = "天猫网", dataurl = "http://m.tmall.com/" });
+            file.Add(new Info() { text = "淘宝网", dataurl = "http://h5.m.taobao.com/" });
             file.Add(new Info() { text = "京东", dataurl = "http://m.jd.com/" });
             file.Add(new Info() { text = "苏宁易购", dataurl = "http://m.suning.com/" });
             file.Add(new Info() { text = "拍拍网", dataurl = "http://www.paipai.com/m2/index.html" });
-                
-            file.Add(new Info() { text = "蘑菇街", dataurl = "http://m.mogujie.com/" });
-          
+            file.Add(new Info() { text = "当当网", dataurl = "http://m.dangdang.com/" });
             file.Add(new Info() { text = "国美在线", dataurl = "http://m.gome.com.cn/" });
             file.Add(new Info() { text = "1号店", dataurl = "http://m.yhd.com/" });
             file.Add(new Info() { text = "亚马逊", dataurl = "http://www.amazon.cn/" });
             file.Add(new Info() { text = "唯品会", dataurl = "http://m.vip.com/" });
             file.Add(new Info() { text = "聚划算", dataurl = "http://jhs.m.taobao.com/m/index.htm" });
-            file.Add(new Info() { text = "当当网", dataurl = "http://m.dangdang.com/" });
             file.Add(new Info() { text = "易迅网", dataurl = "http://m.51buy.com/t" });
+            file.Add(new Info() { text = "蘑菇街", dataurl = "http://m.mogujie.com/" });         
+          
+           
             //file.Add(new Info() { text = "e龙旅行", dataurl = "http://m.elong.com/" });
             // file.Add(new Info() { text = "酒仙网", dataurl = "http://m.jiuxian.com/" });
             //   file.Add(new Info() { text = "美丽说", dataurl = "http://m.meilishuo.com" });
             file.Add(new Info() { text = "糯米网", dataurl = "http://m.nuomi.com/" });
+            file.Add(new Info() { text = "天猫网", dataurl = "http://wap.tmall.com/" });
+          
+          
             //file.Add(new Info() { text = "唯品会", dataurl = "http://m.vip.com/" });
             //file.Add(new Info() { text = "小荷特卖", dataurl = "http://m.xiaoher.com/" });
             //file.Add(new Info() { text = "聚美优品", dataurl = "http://m.jumei.com/" });
